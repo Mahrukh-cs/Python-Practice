37e6f8fc950145200f65b065f515ce828b0bb202
Mahrukh Mushtaq <mahrukh.urooj.cs@gmail.com>
2026-06-29 22:44:52 +0500

    Day 2 practice - Week 1 lecture practice

---
+++ Week1 lecture practice/compare.py
@@ -0,0 +1,44 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# # if
+# if x < y:
+#     print("x is smaller than y")
+# if x > y:
+#     print("x is greater than y")
+# if x == y:
+#     print("x is equal to y")
+
+# # if elif
+# if x < y:
+#     print("x is smaller than y")
+# elif x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x is equal to y")
+
+# # if elif else
+# if x < y:
+#     print("x is smaller than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+# # or 
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+# != 
+# if x != y:
+#     print("x is not equal to y")
+# else: 
+#     print("x is equal to y")
+
+# ==
+if x == y:
+    print("x is equal to y")
+else: 
+    print("x is not equal to y")
